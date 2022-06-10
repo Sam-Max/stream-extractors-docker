68618793a0a49ebf471e13beb60320459d72f238
samagd <sam.agd@outlook.com>
2022-06-10 17:00:46 +0200

    feat: create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,9 @@
+class Config(object):
+     
+    # Get a bot token from botfather
+    BOT_TOKEN = ""
+    # Get from my.telegram.org
+    APP_ID = 0
+    API_HASH = ""
+    # Users who are authorized to use the bot separated by spaces
+    AUTH_USERS = "0"
