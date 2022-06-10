e63f275c4f6b34f7ee4fa27eb3067550f4268c85
samagd <sam.agd@outlook.com>
2022-06-10 17:24:16 +0200

    chore: delete sample_config.py

--- sample_config.py
+++
@@ -1,9 +0,0 @@
-class Config(object):
-     
-    # Get a bot token from botfather
-    BOT_TOKEN = ""
-    # Get from my.telegram.org
-    APP_ID = 0
-    API_HASH = ""
-    # Users who are authorized to use the bot separated by spaces
-    AUTH_USERS = "0"
